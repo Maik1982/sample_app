4b73865be27b3bc0c51017103b99b295ebd77b5c
Maik <m-tappe@t-online.de>
2010-07-30 15:24:06 +0200

    Finished first cut of the User model

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-
+require 'webrat'
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
@@ -38,4 +38,8 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+  
+  Webrat.configure do |config|
+    config.mode = :rails
+  end
 end
